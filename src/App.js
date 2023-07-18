89ab7ba83529eafbb2c0baccd55588d71e86b424
sk ashfak <skashfak18@gmail.com>
2023-07-18 11:05:05 +0530

    Documenttation web

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Documentation from "./Documentation/Documant"
+import  "./Documentation/DocumentStyle.css"
+
+
+
+
+
+
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+    <Documentation/>
+    </div>
+
+  )
+}
+
+
+
+export default App;
---
+++ src/Documentation/Documant.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+
+export default function Documant() {
+    return (
+        <body>
+
+
+
+            <header>
+                <nav>
+                    <h1>Javascribe Documentation</h1>
+                    <ul>
+
+                        <a href="#Introduction"><li>Introductio</li></a>
+                        <a href="#What_you_should_alredy_know"><li>What you should alredy know</li></a>
+                        <a href="#Javascribe_and_java"><li>Javascribe and java</li></a>
+                        <a href="#Variable"><li>Variable</li></a>
+                        <a href="#Hello_World"><li>Hello World</li></a>
+
+                    </ul>
+                </nav>
+            </header>
+
+            <main className='inform'>
+                <section id='Introduction' className='intro'>
+                    <h2>Introduction</h2>
+                    <article>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae totam aspernatur sit, Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut quasi beatae numquam necessitatibus libero culpa ducimus eos minima quisquam ipsam consequuntur eveniet sunt quo assumenda magni recusandae, illo ipsa architecto labore. Perferendis, non nisi necessitatibus dolorem doloribus ducimus libero hic repudiandae nostrum. Maiores veniam temporibus incidunt sint corporis, voluptate ducimus? harum libero perspiciatis ratione numquam dolorem veniam quis doloremque omnis illum quo placeat.</article>
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, quod architecto consequuntur tempore quae magnam illum maxime cum impedit libero!</li>
+                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur quasi animi quis quaerat facere cum quibusdam illo porro similique dicta?</li>
+                    </ul>
+                </section>
+
+                <section id='What_you_should_alredy_know' className='intro'>
+                    <h2>What you should alredy know</h2>
+                    <article>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae totam aspernatur sit, Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut quasi beatae numquam necessitatibus libero culpa ducimus eos minima quisquam ipsam consequuntur eveniet sunt quo assumenda magni recusandae, illo ipsa architecto labore. Perferendis, non nisi necessitatibus dolorem doloribus ducimus libero hic repudiandae nostrum. Maiores veniam temporibus incidunt sint corporis, voluptate ducimus? harum libero perspiciatis ratione numquam dolorem veniam quis doloremque omnis illum quo placeat.</article>
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, quod architecto consequuntur tempore quae magnam illum maxime cum impedit libero!</li>
+                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur quasi animi quis quaerat facere cum quibusdam illo porro similique dicta?</li>
+                    </ul>
+                </section>
+
+
+
+                <section id='Javascribe_and_java' className='intro'>
+                    <h2>Javascribe and java</h2>
+                    <article>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae totam aspernatur sit, Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut quasi beatae numquam necessitatibus libero culpa ducimus eos minima quisquam ipsam consequuntur eveniet sunt quo assumenda magni recusandae, illo ipsa architecto labore. Perferendis, non nisi necessitatibus dolorem doloribus ducimus libero hic repudiandae nostrum. Maiores veniam temporibus incidunt sint corporis, voluptate ducimus? harum libero perspiciatis ratione numquam dolorem veniam quis doloremque omnis illum quo placeat.</article>
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, quod architecto consequuntur tempore quae magnam illum maxime cum impedit libero!</li>
+                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur quasi animi quis quaerat facere cum quibusdam illo porro similique dicta?</li>
+                    </ul>
+                </section>
+
+
+
+                <section id='Variable' className='intro'>
+                    <h2>Variable</h2>
+                    <article>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae totam aspernatur sit, Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut quasi beatae numquam necessitatibus libero culpa ducimus eos minima quisquam ipsam consequuntur eveniet sunt quo assumenda magni recusandae, illo ipsa architecto labore. Perferendis, non nisi necessitatibus dolorem doloribus ducimus libero hic repudiandae nostrum. Maiores veniam temporibus incidunt sint corporis, voluptate ducimus? harum libero perspiciatis ratione numquam dolorem veniam quis doloremque omnis illum quo placeat.</article>
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, quod architecto consequuntur tempore quae magnam illum maxime cum impedit libero!</li>
+                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur quasi animi quis quaerat facere cum quibusdam illo porro similique dicta?</li>
+                    </ul>
+                </section>
+
+                <section id='Hello_World' className='intro'>
+                    <h2>Hello World</h2>
+                    <article>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae totam aspernatur sit, Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut quasi beatae numquam necessitatibus libero culpa ducimus eos minima quisquam ipsam consequuntur eveniet sunt quo assumenda magni recusandae, illo ipsa architecto labore. Perferendis, non nisi necessitatibus dolorem doloribus ducimus libero hic repudiandae nostrum. Maiores veniam temporibus incidunt sint corporis, voluptate ducimus? harum libero perspiciatis ratione numquam dolorem veniam quis doloremque omnis illum quo placeat.</article>
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, quod architecto consequuntur tempore quae magnam illum maxime cum impedit libero!</li>
+                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur quasi animi quis quaerat facere cum quibusdam illo porro similique dicta?</li>
+                    </ul>
+                </section>
+
+            </main>
+
+
+
+        </body>
+    )
+}
